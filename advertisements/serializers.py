4ee38d3d0d2e8cda0c10d4794c94da1b39e149cf
denis burnashev <kardenman@gmail.com>
2022-02-03 21:22:07 +0600

    permissons

---
+++ advertisements/serializers.py
@@ -0,0 +1,58 @@
+from django.contrib.auth.models import User
+from .models import AdvertisementStatusChoices
+from rest_framework import serializers
+from .permissions import IsOwnerOrReadOnly
+
+
+from advertisements.models import Advertisement
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer для пользователя."""
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name',
+                  'last_name',)
+
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+    """Serializer для объявления."""
+
+    creator = UserSerializer(
+        read_only=True,
+    )
+
+    class Meta:
+        model = Advertisement
+        fields = ('id', 'title', 'description', 'creator',
+                  'status', 'created_at', )
+
+    def create(self, validated_data):
+        """Метод для создания"""
+
+        # Простановка значения поля создатель по-умолчанию.
+        # Текущий пользователь является создателем объявления
+        # изменить или переопределить его через API нельзя.
+        # обратите внимание на `context` – он выставляется автоматически
+        # через методы ViewSet.
+        # само поле при этом объявляется как `read_only=True`
+        validated_data["creator"] = self.context["request"].user
+        return super().create(validated_data)
+
+    def validate(self, data):
+        """Метод для валидации. Вызывается при создании и обновлении."""
+        advertisements = Advertisement.objects.all()
+        adv_open = []
+        counting = []
+        for advertisement in advertisements:
+            if advertisement.creator == self.context["request"].user:
+                adv_open.append(advertisement)
+        for status in adv_open:
+            if status.status == AdvertisementStatusChoices.OPEN:
+                counting.append(status)
+        if len(counting) >= 10:
+            raise serializers.ValidationError('У вас не может быть больше 10 открытых объявлений.')
+
+        # TODO: добавьте требуемую валидацию
+        return data
---
+++ advertisements/views.py
@@ -0,0 +1,25 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from .models import Advertisement
+from .serializers import AdvertisementSerializer
+from .permissions import IsOwnerOrReadOnly
+from .filters import filters, AdvertisementFilter
+
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
+    #   сериализаторов и фильтров
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_class = AdvertisementFilter
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update"]:
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+        return []
